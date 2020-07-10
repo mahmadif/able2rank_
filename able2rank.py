02c3b7e535b1e4a09a68d13672f3b320deac1abe
Mohsen <40457356+mahmadif@users.noreply.github.com>
2020-07-11 01:14:08 +0200

    Update able2rank.py

--- able2rank.py
+++ able2rank.py
@@ -4,6 +4,45 @@ from itertools import combinations as comb
 def combn(m, n):
     return np.array(list(comb(range(m), n)))
 
+def Borda(mat):
+    np.fill_diagonal(mat, 1)
+    mat = mat/(mat+mat.T)
+    np.fill_diagonal(mat, 0)
+    
+    return np.sum(mat, axis=1)
+
+def BTL(Data, probs=False, max_iter=10**5):
+    '''
+    computes the parameters using maximum likelihood principle.
+    This function is adapted from the Matlab version provided by David Hunter
+    http://personal.psu.edu/drh20/code/btmatlab
+    '''
+    wm = Data
+    if probs:
+        np.fill_diagonal(wm, 1)
+        wm = wm/(wm+wm.T)
+        np.fill_diagonal(wm, 0)
+    n = wm.shape[0]
+    nmo = n-1
+    pi = np.ones(nmo, dtype=float)
+    gm = (wm[:,range(nmo)]).T + wm[range(nmo),:]
+    wins = np.sum(wm[range(nmo),], axis=1)
+    gind = gm>0
+    z = np.zeros((nmo,n))
+    pisum = z
+    for _ in range(max_iter):
+        pius = np.repeat(pi, n).reshape(nmo, -1)
+        piust = (pius[:,range(nmo)]).T
+        piust = np.column_stack((piust, np.repeat(1,nmo)))
+        pisum[gind] = pius[gind]+piust[gind]
+        z[gind] = gm[gind] / pisum[gind]
+        newpi = wins / np.sum(z, axis=1)
+        if np.linalg.norm(newpi - pi, ord=np.inf) <= 1e-6:
+            newpi = np.append(newpi, 1)
+            return newpi/sum(newpi)
+        pi = newpi
+    raise RuntimeError('did not converge')
+
 '''
 AB: numpy array where each row (instance) is \in [-1,1]^d
 CD: numpy array where each row (instance) is \in [-1,1]^d
